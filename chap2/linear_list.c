bd13f1ffe67c4d35292409ebd5ac8901a781255c
43529 <43529and43529@gmail.com>
2022-01-20 00:13:02 +0800

    函数定义

---
+++ chap2/linear_list.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int InitList(&L) {
+}
+int DestroyList(&L) {
+}
+int ClearList(&L) {
+}
+int ListEmpty(&L) {
+}
+int ListLength(&L) {
+}
+int GetElem(L, i, &e) {
+}
+int LocateElem(L, e, compare()) {
+}
+int PriorElem(L, cur e, &pre e) {
+}
+int NextElem(L, cur e, &next e) {
+}
+int ListInsert(&L, i, e) {
+}
+int ListDelete(&L, i, &e) {
+}
+int ListTraverse(L, visit()) {
+}
+int main(){
+    printf("勇敢牛牛，不怕困难\n");
+    return 0;
+}
