70b9c199ee5639c84d23a3d3ac9d2805758e4a58
43529 <43529and43529@gmail.com>
2022-01-24 00:24:12 +0800

    今日份复健（摆烂）C语言，希望明天能把C语言顺序线性表部分整完抓紧开始下一个链式表

--- chap2/linear_list.c
+++ chap2/linear_list.c
@@ -15,14 +15,24 @@ int InitList(SqList* L) {
     (*L).listsize = LIST_INIT_SIZE;
     return 0;
 }
-//int DestroyList(&L) {
-//}
-//int ClearList(&L) {
-//}
-//int ListEmpty(&L) {
-//}
-//int ListLength(&L) {
-//}
+int DestroyList(SqList* L) {
+}
+int ClearList(SqList* L) {
+    printf("*L.length:%d\n\n\n", (*L).length);
+    for(int i=0;i<3;i++) {
+        *((*L).elem+i) = 0;
+    }
+    return 0;
+}
+int ListEmpty(SqList* L) {
+    if((*L).length = 0){
+        return 0;
+    }
+    return 1;
+}
+int ListLength(SqList* L) {
+    return (*L).length;
+}
 //int GetElem(L, i, &e) {
 //}
 //int LocateElem(L, e, compare()) {
@@ -49,6 +59,11 @@ int main(){
     list.elem[0] = 55;
     list.elem[1] = 66;
     list.elem[2] = 77;
+    list.length = 3;
+    int list_length = 0;
+    list_length = ListLength(&list);
+    printf("list_length:%d\n", list_length);
+    ClearList(&list);
     for(int i=0;i<3;i++) {
         printf("%d\n", list.elem[i]);
     }
