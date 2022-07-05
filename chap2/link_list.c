e879217e518b0bcc519c9eb92a1f5a9432c93d3f
dongxiang <dongxiang39@cnpc.com.cn>
2022-07-05 09:45:52 +0800

    chapter2

--- chap2/link_list.c
+++ chap2/link_list.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+{
+   
+}
 typedef int ElemType;
 typedef struct LNode {
     ElemType data;
@@ -10,11 +13,12 @@ typedef struct LNode {
 int init_node(LinkNode* L) {
     *L = (LinkNode)malloc(sizeof(LNode));
     (*L)->next = NULL;
+	(*L)->data = -1;
     return 0;
 }
 void creat_list_head(LinkNode* L, int n) {
     init_node(L);
-    printf("请输入%d个整型数:\n", n);
+    printf("input %d integer:\n", n);
     for(int i=0;i<n;i++) {
        LinkNode li;
        LinkNode list;
@@ -25,12 +29,12 @@ void creat_list_head(LinkNode* L, int n) {
        list->next = (*L)->next;
        (*L)->next = list;
     }
-    printf("输入结束！\n");
+    printf("finish.\n");
 }
 void print_list(LinkNode L) {
     LinkNode list; 
     list = L;
-    printf("目前的队列数据如下:\n");
+    printf("current data:\n");
     while(list->next != NULL) {
         printf("%d\t",list->data);
         list = list->next;
